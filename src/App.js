a79d963adb058ba03527b9eec0673cf026273bd7
HassanKhan123 <hk2602809@gmail.com>
2021-11-16 12:21:02 +0500

    useToggle hook created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ToggleComponent from './useToggle/ToggleComponent';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <ToggleComponent />
+    </div>
+  );
+};
+
+export default App;
